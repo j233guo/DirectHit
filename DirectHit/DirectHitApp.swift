5819e20f7787658957e2097c403369fdfa479f0e
Jiaming Guo <j23guo@gmail.com>
2022-06-12 16:58:19 -0400

    Initial Commit

---
+++ DirectHit/DirectHitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectHitApp.swift
+//  DirectHit
+//
+//  Created by Jiaming Guo on 2022-06-12.
+//
+
+import SwiftUI
+
+@main
+struct DirectHitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
